f8777fe90dc2ac2cd8790886e27b82d41638acb6
tarun-cumulations <tarun.t@cumulations.com>
2023-12-03 03:24:32 +0530

    mofifying filter API

--- app.py
+++ app.py
@@ -24,25 +24,6 @@ def get_db_connection():
 
 #  http://127.0.0.1:5000/applications/search?applicantId=7
 
-# @app.route('/applications/search', methods=['GET'])
-# def search_applications():
-#     applicant_id = request.args.get('applicantId', type=int)
-
-#     if not applicant_id:
-#         return jsonify({'error': 'Applicant ID is required'}), 400
-
-#     conn = get_db_connection()
-#     cursor = conn.cursor()
-
-#     cursor.execute('SELECT * FROM application WHERE applicant_id = %s', (applicant_id,))
-#     applications = cursor.fetchall()
-
-#     cursor.close()
-#     conn.close()
-
-#     return jsonify(applications)
-
-
 @app.route('/applications/search', methods=['GET'])
 def search_applications():
     applicant_id = request.args.get('applicantId', type=int)
@@ -106,99 +87,53 @@ def filter_applications():
     if not start_date or not end_date:
         return jsonify({'error': 'Both startDate and endDate are required'}), 400
 
-    start_date = format_date(start_date)
-    end_date = format_date(end_date)
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
 
-    conn = get_db_connection()
-    cursor = conn.cursor()
+        cursor.execute("""
+            SELECT app.application_id, app.applicant_id, ap.applicant_name, app.category, app.load_applied, 
+                   app.date_of_application, app.date_of_approval, app.modified_date, app.status, 
+                   ap.govtid_type, rev.review_id, revr.reviewer_name, rev.reviewer_comments
+            FROM application app
+            JOIN applicant ap ON app.applicant_id = ap.applicant_id
+            LEFT JOIN review rev ON app.application_id = rev.application_id
+            LEFT JOIN reviewer revr ON rev.reviewer_id = revr.reviewer_id
+            WHERE app.date_of_application BETWEEN %s AND %s
+            ORDER BY app.application_id
+        """, (start_date, end_date))
+        applications = cursor.fetchall()
 
-    query = """
-        SELECT * FROM application 
-        WHERE date_of_application BETWEEN %s AND %s
-    """
-    cursor.execute(query, (start_date, end_date))
-    applications = cursor.fetchall()
+        cursor.close()
+        conn.close()
 
-    cursor.close()
-    conn.close()
+        applications_data = [
+            {
+                "application_id": app[0],
+                "applicant_id": app[1],
+                "applicant_name": app[2],
+                "category": app[3],
+                "load_applied": app[4],
+                "date_of_application": app[5].strftime("%Y-%m-%d") if app[5] else None,
+                "date_of_approval": app[6].strftime("%Y-%m-%d") if app[6] else None,
+                "modified_date": app[7].strftime("%Y-%m-%d") if app[7] else None,
+                "status": app[8],
+                "govt_id_type": app[9],
+                "review_id": app[10],
+                "reviewer_name": app[11],
+                "reviewer_comments": app[12]
+            }
+            for app in applications
+        ]
 
-    return jsonify(applications)
+        return jsonify(applications_data)
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
 
 
 # http://127.0.0.1:5000/applications
-
-# @app.route('/applications', methods=['GET'])
-# def get_applications():
-#     page = request.args.get('page', 1, type=int)
-#     per_page = request.args.get('per_page', 10, type=int)
-
-#     conn = get_db_connection()
-#     cursor = conn.cursor()
-#     offset = (page - 1) * per_page
-
-#     cursor.execute('SELECT COUNT(*) FROM application')
-#     total = cursor.fetchone()[0]
-
-#     cursor.execute('SELECT * FROM application ORDER BY application_id LIMIT %s OFFSET %s', (per_page, offset))
-#     applications = cursor.fetchall()
-
-#     cursor.close()
-#     conn.close()
-
-#     return jsonify({
-#         'total': total,
-#         'page': page,
-#         'per_page': per_page,
-#         'data': applications
-#     })
-
-
-# @app.route('/applications', methods=['GET'])
-# def get_applications():
-#     page = request.args.get('page', 1, type=int)
-#     per_page = request.args.get('per_page', 10, type=int)
-
-#     conn = get_db_connection()
-#     cursor = conn.cursor()
-#     offset = (page - 1) * per_page
-
-#     cursor.execute('SELECT COUNT(*) FROM application')
-#     total = cursor.fetchone()[0]
-
-#     cursor.execute("""
-#         SELECT app.application_id, app.applicant_id, app.category, app.load_applied, app.date_of_application, app.date_of_approval, app.modified_date, app.status, ap.govtid_type
-#         FROM application app
-#         JOIN applicant ap ON app.applicant_id = ap.applicant_id
-#         ORDER BY app.application_id 
-#         LIMIT %s OFFSET %s
-#     """, (per_page, offset))
-#     applications = cursor.fetchall()
-
-#     cursor.close()
-#     conn.close()
-
-#     applications_data = [
-#         {
-#             "application_id": app[0],
-#             "applicant_id": app[1],
-#             "category": app[2],
-#             "load_applied": app[3],
-#             "date_of_application": app[4],
-#             "date_of_approval": app[5],
-#             "modified_date": app[6],
-#             "status": app[7],
-#             "govt_id_type": app[8]
-#         }
-#         for app in applications
-#     ]
-
-#     return jsonify({
-#         'total': total,
-#         'page': page,
-#         'per_page': per_page,
-#         'data': applications_data
-#     })
-
 
 @app.route('/applications', methods=['GET'])
 def get_applications():
@@ -255,114 +190,6 @@ def get_applications():
     })
 
 
-# @app.route('/applications', methods=['GET'])
-# def get_applications():
-#     page = request.args.get('page', 1, type=int)
-#     per_page = request.args.get('per_page', 10, type=int)
-
-#     conn = get_db_connection()
-#     cursor = conn.cursor()
-#     offset = (page - 1) * per_page
-
-#     cursor.execute('SELECT COUNT(*) FROM application')
-#     total = cursor.fetchone()[0]
-
-#     cursor.execute("""
-#         SELECT app.application_id, app.applicant_id, ap.applicant_name, app.category, app.load_applied, 
-#                app.date_of_application, app.date_of_approval, app.modified_date, app.status, 
-#                ap.govtid_type, rev.review_id, revr.reviewer_name
-#         FROM application app
-#         JOIN applicant ap ON app.applicant_id = ap.applicant_id
-#         LEFT JOIN review rev ON app.application_id = rev.application_id
-#         LEFT JOIN reviewer revr ON rev.reviewer_id = revr.reviewer_id
-#         ORDER BY app.application_id 
-#         LIMIT %s OFFSET %s
-#     """, (per_page, offset))
-#     applications = cursor.fetchall()
-
-#     cursor.close()
-#     conn.close()
-
-#     applications_data = [
-#         {
-#             "application_id": app[0],
-#             "applicant_id": app[1],
-#             "applicant_name": app[2],  # Added applicant_name
-#             "category": app[3],
-#             "load_applied": app[4],
-#             "date_of_application": app[5].strftime("%Y-%m-%d") if app[5] else None,
-#             "date_of_approval": app[6].strftime("%Y-%m-%d") if app[6] else None,
-#             "modified_date": app[7].strftime("%Y-%m-%d") if app[7] else None,
-#             "status": app[8],
-#             "govt_id_type": app[9],
-#             "review_id": app[10],
-#             "reviewer_name": app[11]
-#         }
-#         for app in applications
-#     ]
-
-#     return jsonify({
-#         'total': total,
-#         'page': page,
-#         'per_page': per_page,
-#         'data': applications_data
-#     })
-
-
-
-
-# @app.route('/applications', methods=['GET'])
-# def get_applications():
-#     page = request.args.get('page', 1, type=int)
-#     per_page = request.args.get('per_page', 10, type=int)
-
-#     conn = get_db_connection()
-#     cursor = conn.cursor()
-#     offset = (page - 1) * per_page
-
-#     cursor.execute('SELECT COUNT(*) FROM application')
-#     total = cursor.fetchone()[0]
-
-#     cursor.execute("""
-#         SELECT app.application_id, app.applicant_id, app.category, app.load_applied, app.date_of_application, 
-#                app.date_of_approval, app.modified_date, app.status, ap.govtid_type, 
-#                rev.review_id, revr.reviewer_name
-#         FROM application app
-#         JOIN applicant ap ON app.applicant_id = ap.applicant_id
-#         LEFT JOIN review rev ON app.application_id = rev.application_id
-#         LEFT JOIN reviewer revr ON rev.reviewer_id = revr.reviewer_id
-#         ORDER BY app.application_id 
-#         LIMIT %s OFFSET %s
-#     """, (per_page, offset))
-#     applications = cursor.fetchall()
-
-#     cursor.close()
-#     conn.close()
-
-#     applications_data = [
-#         {
-#             "application_id": app[0],
-#             "applicant_id": app[1],
-#             "category": app[2],
-#             "load_applied": app[3],
-#             "date_of_application": app[4],
-#             "date_of_approval": app[5],
-#             "modified_date": app[6],
-#             "status": app[7],
-#             "govt_id_type": app[8],
-#             "review_id": app[9],
-#             "reviewer_name": app[10]
-#         }
-#         for app in applications
-#     ]
-
-#     return jsonify({
-#         'total': total,
-#         'page': page,
-#         'per_page': per_page,
-#         'data': applications_data
-#     })
-
 
 # for below API 
 
@@ -387,32 +214,7 @@ def get_applications():
 # }
 
 
-# @app.route('/applications/edit/<int:id>', methods=['PUT'])
-# def edit_application(id):
-#     data = request.json
-#     if 'load_applied' in data and data['load_applied'] > 200:
-#         return jsonify({'error': 'Load applied should not exceed 200 KV'}), 400
 
-#     # Fields that should not be changed
-#     immutable_fields = ['date_of_application', 'govtid_type', 'id_number']
-#     if any(field in data for field in immutable_fields):
-#         return jsonify({'error': 'Some fields cannot be changed'}), 400
-
-#     conn = get_db_connection()
-#     cursor = conn.cursor()
-
-#     columns = ', '.join([f"{key} = %s" for key in data])
-#     values = list(data.values()) + [id]
-
-#     query = f'UPDATE application SET {columns} WHERE application_id = %s'
-#     cursor.execute(query, values)
-
-#     conn.commit()
-
-#     cursor.close()
-#     conn.close()
-
-#     return jsonify({'success': 'Application updated successfully'})
 
 
 
